a2b625c03a69db262a547dbec618745d43c8d56e
Mahad Aziz <azizmahad@gmail.com>
2022-01-06 19:44:39 -0500

    Updated files

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,40 +82,40 @@ function navHighlighter()
 }
 
 /*=============== PORTFOLIO ITEM FILTER ===============*/
-const filterContainer = document.querySelector(".portfolio-filter-inner"),
-      filterBtns = filterContainer.children,
-      totalFilterBtn = filterBtns.length,
-      portfolioItems = document.querySelectorAll(".portfolio-item"),
-      totalPortfolioItem = portfolioItems.length;
+// const filterContainer = document.querySelector(".portfolio-filter-inner"),
+//       filterBtns = filterContainer.children,
+//       totalFilterBtn = filterBtns.length,
+//       portfolioItems = document.querySelectorAll(".portfolio-item"),
+//       totalPortfolioItem = portfolioItems.length;
       
-      for(let i=0; i<totalFilterBtn; i++)
-      {
-        filterBtns[i].addEventListener("click", function()
-        {
-          filterContainer.querySelector(".active").classList.remove("active");
-          this.classList.add("active");
+//       for(let i=0; i<totalFilterBtn; i++)
+//       {
+//         filterBtns[i].addEventListener("click", function()
+//         {
+//           filterContainer.querySelector(".active").classList.remove("active");
+//           this.classList.add("active");
 
-          const filterValue = this.getAttribute("data-filter");
-          for(let k=0; k<totalPortfolioItem; k++)
-          {
-            if(filterValue === portfolioItems[k].getAttribute("data-category"))
-            {
-              portfolioItems[k].classList.remove("hide");
-              portfolioItems[k].classList.add("show");
-            }
-            else
-            {
-              portfolioItems[k].classList.add("hide");
-              portfolioItems[k].classList.remove("show");
-            }
-            if(filterValue === "all")
-            {
-              portfolioItems[k].classList.remove("hide");
-              portfolioItems[k].classList.add("show");
-            }
-          }
-        })
-      }
+//           const filterValue = this.getAttribute("data-filter");
+//           for(let k=0; k<totalPortfolioItem; k++)
+//           {
+//             if(filterValue === portfolioItems[k].getAttribute("data-category"))
+//             {
+//               portfolioItems[k].classList.remove("hide");
+//               portfolioItems[k].classList.add("show");
+//             }
+//             else
+//             {
+//               portfolioItems[k].classList.add("hide");
+//               portfolioItems[k].classList.remove("show");
+//             }
+//             if(filterValue === "all")
+//             {
+//               portfolioItems[k].classList.remove("hide");
+//               portfolioItems[k].classList.add("show");
+//             }
+//           }
+//         })
+//       }
 
 /*=============== THEME/DISPLAY CUSTOMIZATION ===============*/
 const theme = document.querySelector("#theme-button");
